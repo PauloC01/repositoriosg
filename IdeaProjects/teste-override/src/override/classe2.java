251f5ca087be1c30af34d46c2558935642dac7d6
PauloC01 <paulhenricamargo@outlook.com>
2020-07-22 09:24:33 -0300

    1.0.1 added

--- IdeaProjects/teste-override/src/override/classe2.java
+++ IdeaProjects/teste-override/src/override/classe2.java
@@ -5,6 +5,7 @@ public class classe2 extends classe1{
     @Override //essa anotação sobrescreve um metodo q
     public void VerificaSaldo() {
         System.out.println("Manteiga");
+        System.out.println("Manteiga e pão");
 
     }
 }
