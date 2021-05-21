e3acc8d962823816d636b1c0c477ac3748bc1bf7
PauloC01 <68249297+PauloC01@users.noreply.github.com>
2021-05-21 11:52:27 -0300

    Delete IdeaProjects/lanchessg directory

--- IdeaProjects/lanchessg/src/main/java/com/sg/lanchessg/Repositories/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sg.lanchessg.Repositories;
-
-import com.sg.lanchessg.Model.Cliente;
-import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Cliente, Integer>  {}
