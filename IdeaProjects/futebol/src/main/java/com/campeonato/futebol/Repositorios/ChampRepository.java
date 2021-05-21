82f4b42b49b8617573d81c7b07c8f23f40e1aa5d
PauloC01 <68249297+PauloC01@users.noreply.github.com>
2021-05-21 14:25:03 -0300

    Delete IdeaProjects/futebol directory

--- IdeaProjects/futebol/src/main/java/com/campeonato/futebol/Repositorios/ChampRepository.java
+++
@@ -1,9 +0,0 @@
-package com.campeonato.futebol.Repositorios;
-
-import com.campeonato.futebol.Model.Campeonato;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChampRepository extends JpaRepository<Campeonato, Integer> {
-}
--- IdeaProjects/futebol/src/main/java/com/campeonato/futebol/Repositorios/JogoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campeonato.futebol.Repositorios;
-
-import com.campeonato.futebol.Model.Jogos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogoRepository extends JpaRepository<Jogos, Integer> {
-}
